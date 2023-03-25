7ece59d8599d348f314e7b06d56de24797ef72bd
Tia Simmons <t.simmons06@yahoo.com>
2023-03-25 18:14:10 -0400

    add cors and update router for server access

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect('mongodb://127.0.0.1:27017/moviesapi');
-        console.log("MongoDB is connected");
+        console.log("successfully connected to server");
         } catch (err) {
             console.log(err.message);
             process.exit(1);
--- mern_app/src/App.js
+++ mern_app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 // import Login from './components/Login';
 import CreateMovie from './components/CreateMovie';
+import ShowMovieList from './components/ShowMovieList';
 // import MovieCard from './components/MovieCard';
 
 
@@ -12,7 +13,7 @@ const App = () => {
       <div>
         <Routes>
           {/* <Route exact path='/login' element={<Login />} /> */}
-          <Route path='/' element={<CreateMovie />} />
+          <Route path='/' element={<ShowMovieList />} />
           <Route path='/addMovie' element={<CreateMovie />} />
         </Routes>
       </div>
